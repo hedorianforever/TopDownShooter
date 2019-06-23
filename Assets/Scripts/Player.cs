c37708eb6664142065f260e57825b77e9207b1ba
rodrigo <rodrigolemes@live.com>
2019-06-23 17:07:36 -0300

    Implemented Player Movement
    Implemented Player movement and Player movement animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private Vector2 moveAmount;
+
+    private float horizontal;
+    private float vertical;
+    private float moveLimiter = 0.7f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        ChangeDirectionFaced();
+        SetPlayerAnimation();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void ChangeDirectionFaced()
+    {
+        //changes direction player is facing, but not when player is idle
+        if (horizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (horizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    private void SetPlayerAnimation()
+    {
+        if (horizontal != 0 || vertical != 0)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        
+        if (horizontal != 0 && vertical != 0)
+        {
+            //limit movement when moving diagonally, so you don't move so fast
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+    }
+}
