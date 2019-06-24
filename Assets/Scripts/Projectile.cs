5a923b595a5bf79ba82b8911239bb207a791cbfc
rodrigo <rodrigolemes@live.com>
2019-06-23 23:27:17 -0300

    Implemented Weapon and Projectile scripts

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] GameObject destroyFX = default;
+
+    private Weapon weapon;
+
+    private void Start()
+    {
+        Invoke("DestroyProjectile", weapon.projectileLifetime);
+    }
+
+    private void Update()
+    {
+        if (weapon == null)
+        {
+            return;
+        }
+
+        transform.Translate(Vector2.right * weapon.projectileSpeed * Time.deltaTime);
+    }
+
+    private void DestroyProjectile()
+    {
+        if (destroyFX != null)
+        {
+            Instantiate(destroyFX, transform.position, Quaternion.identity);
+        } else
+        {
+            Debug.Log("destroyFX missing at projectile " + name);
+        }
+        Destroy(gameObject);
+    }
+
+    public void Init(Weapon projectileWeapon)
+    {
+        weapon = projectileWeapon;
+    }
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab = default;
+    [SerializeField] Transform shotPoint = default;
+    [SerializeField] float timeBetweenShots = 1f;
+
+    public float projectileSpeed = 10f;
+    public float projectileLifetime = 5f;
+
+    private bool isOnCooldown = false;
+
+    private void Update()
+    {
+        LookAtMouse();
+        Shoot();
+    }
+
+    private void LookAtMouse()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDirection = mousePos - transform.position;
+
+        lookDirection.Normalize(); //not really necessary, just showing that it doesn't matter
+
+        transform.up = lookDirection; //transform.up = basically the same as rotation.z
+
+        transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + 90)); //add 90 to final rotation because the weapon is initially looking right;
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isOnCooldown)
+            {
+                StartCoroutine(ShootCoroutine());
+            }
+        }
+    }
+
+    IEnumerator ShootCoroutine()
+    {
+        isOnCooldown = true;
+
+        GameObject projectile = Instantiate(projectilePrefab, shotPoint.position, transform.rotation) as GameObject;
+        projectile.GetComponent<Projectile>().Init(this);
+
+        yield return new WaitForSeconds(timeBetweenShots);
+
+        isOnCooldown = false;
+    }
+}
