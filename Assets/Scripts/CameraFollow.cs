edadae18b0f8605581f160834bb21f5631e72117
rodrigo <rodrigolemes@live.com>
2019-06-24 02:46:33 -0300

    Started implementing the tilemaps
    The scut tilemap looks kinda good, but it does look scaled(because it's originally a 8x8 tileset, not 16x16). Will have to look for another tileset, probably.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform transformToFollow = default;
+    [SerializeField] float speed = 0.125f;
+
+    private void Start()
+    {
+        transform.position = transformToFollow.position;
+    }
+
+    private void Update()
+    {
+        if (transformToFollow != null)
+        {
+            transform.position = Vector2.Lerp(transform.position, transformToFollow.position, speed);
+        }
+    }
+}
