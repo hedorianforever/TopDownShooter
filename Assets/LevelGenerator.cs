f65e2e1410f39c5c0afdd269c1c37f41d432664f
rodrigo <rodrigolemes@live.com>
2019-07-16 00:26:25 -0300

    Added Tilemap Procedural Generation
    Added a lot of testing tilesets, unity extra's tilemaps with rule tiles. Tweaked LevelGenerator's values to try and create a good first level. Everything seems to be working perfectly as of this commit.

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,358 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Pathfinding;
+
+public class LevelGenerator : MonoBehaviour
+{
+    enum gridSpace { empty, floor, wall };
+
+    gridSpace[,] grid;
+    //GameObject[,] gridObjs;
+    List<GameObject> gridObjs = new List<GameObject>();
+    int roomHeight, roomWidth;
+    [SerializeField] Vector2 roomSizeWorldUnits = new Vector2(30, 30);
+    float worldUnitsInOneGridCell = 1;
+
+    struct walker
+    {
+        public Vector2 dir;
+        public Vector2 pos;
+    }
+
+    //Making the walkers spawns more often and turn more often should generally make your rooms wider.
+
+    List<walker> walkers;
+    [SerializeField] float chanceWalkerChangeDir = 0.5f;
+    [SerializeField] float chanceWalkerSpawn = 0.05f;
+    [SerializeField] float chanceWalkerDestroy = 0.05f;
+    [SerializeField] int maxWalkers = 10;
+    [SerializeField] float percentToFill = 0.2f;
+
+    [SerializeField] TileBase floorTile = default;
+    [SerializeField] TileBase wallTile = default;
+    [SerializeField] TileBase emptySpaceTile = default;
+
+    [SerializeField] Tilemap groundTilemap = default;
+    [SerializeField] Tilemap obstacleTilemap = default;
+    [SerializeField] Tilemap spaceTilemap = default;
+    
+
+
+    [SerializeField] bool shouldRemoveSingleWalls = true;
+
+
+    private void Start()
+    {
+        Setup();
+        CreateFloors();
+        CreateWalls();
+        if (shouldRemoveSingleWalls)
+        {
+            RemoveSingleWalls();
+        }
+        SpawnLevel();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ClearGrid();
+            Setup();
+            CreateFloors();
+            CreateWalls();
+            if (shouldRemoveSingleWalls)
+            {
+                RemoveSingleWalls();
+            }
+            SpawnLevel();
+        }
+    }
+
+    void ClearGrid()
+    {
+        foreach (GameObject go in gridObjs)
+        {
+            Destroy(go);
+        }
+        gridObjs.Clear();
+    }
+
+    void Setup()
+    {
+        //Clear the map (ensures we dont overlap)
+        groundTilemap.ClearAllTiles();
+        obstacleTilemap.ClearAllTiles();
+        //find grid size
+        roomHeight = Mathf.RoundToInt(roomSizeWorldUnits.x / worldUnitsInOneGridCell);
+        roomWidth = Mathf.RoundToInt(roomSizeWorldUnits.y / worldUnitsInOneGridCell);
+        //create grid
+        grid = new gridSpace[roomWidth, roomHeight];
+        //set grid's default state 
+        for (int x = 0; x < roomWidth - 1; x++)
+        {
+            for (int y = 0; y < roomHeight - 1; y++)
+            {
+                //make every cell "empty"
+                grid[x, y] = gridSpace.empty;
+            }
+        }
+
+        //set first walker
+        //init list
+        walkers = new List<walker>();
+        //create a walker
+        walker newWalker = new walker();
+        newWalker.dir = RandomDirection();
+        //find center of grid
+        Vector2 spawnPos = new Vector2(Mathf.RoundToInt(roomWidth / 2.0f),
+                                       Mathf.RoundToInt(roomHeight / 2.0f));
+        newWalker.pos = spawnPos;
+        //add walker to list
+        walkers.Add(newWalker);
+    }
+
+    void CreateFloors()
+    {
+        int iterations = 0; //loop will not run forever
+        do
+        {
+            //create floor at every walker's position
+            foreach (walker myWalker in walkers)
+            {
+                grid[(int)myWalker.pos.x, (int)myWalker.pos.y] = gridSpace.floor;
+            }
+
+            //chance to destroy walker
+            int numberChecks = walkers.Count; //might modify count while in this loop
+            for (int i = 0; i < numberChecks; i++)
+            {
+                //only if it's not the only one, and at a low chance
+                if (Random.value < chanceWalkerDestroy && walkers.Count > 1)
+                {
+                    walkers.RemoveAt(i);
+                    break; //only destroy one per iteration
+                }
+            }
+
+            //chance: walker pick a new direction
+            for (int i = 0; i < walkers.Count; i++)
+            {
+                if (Random.value < chanceWalkerChangeDir)
+                {
+                    walker thisWalker = walkers[i];
+                    thisWalker.dir = RandomDirection();
+                    walkers[i] = thisWalker;
+                }
+            }
+
+            //chance: spawn new walker
+            numberChecks = walkers.Count; //might modify while in this loop
+            for (int i = 0; i < numberChecks; i++)
+            {
+                //only if # of walkers < max, and at a low chance
+                if (Random.value < chanceWalkerSpawn && walkers.Count < maxWalkers)
+                {
+                    //create a walker at current walker's position
+                    walker newWalker = new walker();
+                    newWalker.dir = RandomDirection();
+                    newWalker.pos = walkers[i].pos;
+                    walkers.Add(newWalker);
+                }
+            }
+
+            //move walkers
+            for (int i = 0; i < walkers.Count; i++)
+            {
+                walker thisWalker = walkers[i];
+                thisWalker.pos += thisWalker.dir;
+                walkers[i] = thisWalker;
+            }
+
+            //avoid boarder of grid
+            for (int i = 0; i < walkers.Count; i++)
+            {
+                walker thisWalker = walkers[i];
+                //clamp x,y to leave a 1 space border: leave room for walls
+                thisWalker.pos.x = Mathf.Clamp(thisWalker.pos.x, 1, roomWidth - 2);
+                thisWalker.pos.y = Mathf.Clamp(thisWalker.pos.y, 1, roomHeight - 2);
+                walkers[i] = thisWalker;
+            }
+
+            //check to exit loop
+            if ((float)NumberOfFloors() / (float)grid.Length > percentToFill)
+            {
+                break;
+            }
+            iterations++;
+        } while (iterations < 100000);
+    }
+
+    void CreateWalls()
+    {
+        //loop through every grid space
+        for (int x = 0; x < roomWidth - 1; x++)
+        {
+            for (int y = 0; y < roomHeight - 1; y++)
+            {
+                //if there's a floor, check the spaces around it 
+                if (grid[x, y] == gridSpace.floor)
+                {
+                    //if any surrounding spaces are empty, place a wall
+                    if (grid[x, y + 1] == gridSpace.empty)
+                    {
+                        grid[x, y + 1] = gridSpace.wall;
+                    }
+                    if (grid[x, y - 1] == gridSpace.empty)
+                    {
+                        grid[x, y - 1] = gridSpace.wall;
+                    }
+                    if (grid[x + 1, y] == gridSpace.empty)
+                    {
+                        grid[x + 1, y] = gridSpace.wall;
+                    }
+                    if (grid[x - 1, y] == gridSpace.empty)
+                    {
+                        grid[x - 1, y] = gridSpace.wall;
+                    }
+                }
+            }
+        }
+    }
+
+    //could be a create obstacle where there is a single wall instead
+    void RemoveSingleWalls()
+    {
+        //loop through every grid space
+        for (int x = 0; x < roomWidth - 1; x++)
+        {
+            for (int y = 0; y < roomHeight - 1; y++)
+            {
+                //if there's a wall, check the spaces around it
+                if (grid[x, y] == gridSpace.wall)
+                {
+                    //assume all spaces around the wall are floors
+                    bool allFloors = true;
+                    //check each side to see if they are all floors 
+                    for (int checkX = -1; checkX <= 1; checkX++)
+                    {
+                        for (int checkY = -1; checkY <= 1; checkY++)
+                        {
+                            if (x + checkX < 0 || x + checkX > roomWidth - 1 ||
+                                y + checkY < 0 || y + checkY > roomHeight - 1)
+                            {
+                                //skip checks that are out of range
+                                continue;
+                            }
+                            if ((checkX != 0 && checkY != 0) || (checkX == 0 && checkY == 0))
+                            {
+                                //skip corners and center
+                                continue;
+                            }
+                            if (grid[x + checkX, y + checkY] != gridSpace.floor)
+                            {
+                                allFloors = false;
+                            }
+                        }
+                    }
+                    //if is single wall
+                    if (allFloors)
+                    {
+                        grid[x, y] = gridSpace.floor;
+                    }
+                }
+            }
+        }
+    }
+
+    Vector2 RandomDirection()
+    {
+        //pick random int between 1 and 3
+        int choice = Mathf.FloorToInt(Random.value * 3.99f);
+        //use that int to choose direction
+        switch (choice)
+        {
+            case 0: return Vector2.down;
+            case 1: return Vector2.left;
+            case 2: return Vector2.up;
+            case 3: return Vector2.right;
+            default: return Vector2.zero;
+        }
+    }
+
+    int NumberOfFloors()
+    {
+        int count = 0;
+        foreach (gridSpace space in grid)
+        {
+            if (space == gridSpace.floor)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    void SpawnLevel()
+    {
+        for (int x = 0; x < roomWidth; x++)
+        {
+            for (int y = 0; y < roomHeight; y++)
+            {
+                switch (grid[x, y])
+                {
+                    case gridSpace.empty:
+                        //Spawn(x, y, emptySpaceTile, spaceTilemap);
+
+                        //for now, I want empty space to be walls
+                        Spawn(x, y, wallTile, obstacleTilemap);
+                        break;
+                    case gridSpace.floor:
+                        Spawn(x, y, floorTile, groundTilemap);
+                        break;
+                    case gridSpace.wall:
+                        //also spawning the floorTile below the wallTile in case the walltile has transparent background
+                        Spawn(x, y, floorTile, groundTilemap);
+                        Spawn(x, y, wallTile, obstacleTilemap);
+                        break;
+                }
+            }
+        }
+        //for some reason it's not working if called immediately; the tilemap must take a while to recalculate everything
+        Invoke("ScanLevel", .1f);
+
+    }
+
+    //void Spawn(float x, float y, GameObject toSpawn)
+    //{
+    //    //find the position to spawn
+    //    Vector2 offset = roomSizeWorldUnits / 2.0f;
+    //    Vector2 spawnPos = new Vector2(x, y) * worldUnitsInOneGridCell - offset;
+    //    //spawn object
+    //    GameObject spawnedObj = Instantiate(toSpawn, spawnPos, Quaternion.identity) as GameObject;
+    //    gridObjs.Add(spawnedObj);
+    //}
+
+    void Spawn(float x, float y, TileBase tile, Tilemap tilemap)
+    {
+        Vector2 offset = roomSizeWorldUnits / 2.0f; 
+        Vector2 spawnPos = new Vector2(x, y) * worldUnitsInOneGridCell - offset; //if x and y range from 0 to 30; world units from -15 to 15, for example
+
+        tilemap.SetTile(new Vector3Int((int)spawnPos.x, (int)spawnPos.y, 0), tile);
+    }
+
+    void ScanLevel()
+    {
+        if (AstarPath.active != null)
+        {
+            AstarPath.active.Scan();
+        }
+        else
+        {
+            Debug.LogError("No active AstarPath found in the scene");
+        }
+    }
+
+}
